d954cc4311f033823a7b0564d18a260678ff1cd3
Putra Nugraha <putra.nugraha@dana.id>
2020-08-19 16:42:04 +0700

    Update edit profile driver api to only allow edit name and phone number

--- api/edit_profile_driver.php
+++ api/edit_profile_driver.php
@@ -4,8 +4,6 @@ require '../config/koneksi.php';
 $loginRequest = json_decode(file_get_contents('php://input'));
 $query = "UPDATE `driver` 
     SET `nama_driver`=\"" . $loginRequest->name . "\", 
-    `tim_travel`=\"" . $loginRequest->group . "\", 
-    `email`=\"" . $loginRequest->email . "\", 
     `no_handphone`=\"" . $loginRequest->phoneNumber . "\" 
     WHERE `id`=\"" . $loginRequest->id . "\"";
 $result = mysqli_query($konek, $query);
