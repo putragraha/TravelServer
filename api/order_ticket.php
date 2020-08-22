8fa07807d7e953067a33df53578150fb4fcd8f2c
Putra Nugraha <putra.nugraha@dana.id>
2020-08-22 18:31:05 +0700

    Rename loginRequest to request in order ticket API

--- api/order_ticket.php
+++ api/order_ticket.php
@@ -3,23 +3,23 @@ require '../config/koneksi.php';
 
 const PENDING = "PENDING";
 
-$loginRequest = json_decode(file_get_contents('php://input'));
+$request = json_decode(file_get_contents('php://input'));
 $query = "INSERT INTO `pemesanantiket` (kode_pemesanan, user_id, armada_id, kursi_pesanan, catatan, latitude, longitude, status)
 VALUES (
     \"" . round(microtime(true) * 1000) . "\", 
-    $loginRequest->userId, 
-    $loginRequest->armadaId, 
-    $loginRequest->seatBooked, 
-    \"$loginRequest->note\", 
-    $loginRequest->latitude, 
-    $loginRequest->longitude,
+    $request->userId, 
+    $request->armadaId, 
+    $request->seatBooked, 
+    \"$request->note\", 
+    $request->latitude, 
+    $request->longitude,
     \"" . PENDING . "\"
 )";
 $insertResult = mysqli_query($konek, $query);
 
 $updateArmadaQuery = "UPDATE `armada` 
-    SET `kursi_tersedia`=`kursi_tersedia` - $loginRequest->seatBooked
-    WHERE `id`=\"" . $loginRequest->armadaId . "\"
+    SET `kursi_tersedia`=`kursi_tersedia` - $request->seatBooked
+    WHERE `id`=\"" . $request->armadaId . "\"
     AND `kursi_tersedia` > 0";
 $updateArmadaResult = mysqli_query($konek, $updateArmadaQuery);
 
