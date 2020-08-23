137bcc8269c2122d0f13e1a036982dbc78868b27
Putra Nugraha <putra.nugraha@dana.id>
2020-08-24 06:48:40 +0700

    Add note as editable field

--- api/edit_order_ticket.php
+++ api/edit_order_ticket.php
@@ -1,11 +1,12 @@
 <?php 
 require '../config/koneksi.php';
 
-$loginRequest = json_decode(file_get_contents('php://input'));
+$request = json_decode(file_get_contents('php://input'));
 $query = "UPDATE `pemesanantiket` 
-    SET `latitude`=$loginRequest->latitude, 
-    `longitude`=$loginRequest->longitude
-    WHERE `kode_pemesanan`=\"" . $loginRequest->orderCode . "\"";
+    SET `latitude`=$request->latitude, 
+    `longitude`=$request->longitude,
+    `catatan`=\"$request->note\"
+    WHERE `kode_pemesanan`=\"" . $request->orderCode . "\"";
 $result = mysqli_query($konek, $query);
 
 echo json_encode(array('success'=>$result > 0)); 
